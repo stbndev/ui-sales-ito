bd2902d6fe107ead2d6e18b6b523c5f174fec717
STBN <stbndev@mefistoles.devil>
2020-02-14 01:14:33 -0600

    ggg

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Productsmodel } from "./../models/productsmodel";
 import { CSTATUS } from "./../config/enums-global.enum";
 import { ConfigService } from "./../config/config-service.service";
 import { Tipos } from "./../config/enums-global.enum";
+import { AddsetComponent } from "./addset/addset.component";
 
 @Component({
   selector: 'app-products',
@@ -16,6 +17,7 @@ export class ProductsComponent implements OnInit {
   productstmp: any = [];
   model: Productsmodel;
   liststatus = CSTATUS;
+
 
   constructor(protected service: ConfigService) { }
 
@@ -36,17 +38,22 @@ export class ProductsComponent implements OnInit {
   // Events
   ProductAddSet() {
     // alert('test');
+    let tmpProduct : Productsmodel;
+    this.service.changeProductsData(tmpProduct);
+
     var element = document.getElementById("divProductAddSet");
-    console.dir(element);
+
     if (element.className === 'hideComponent') {
       element.classList.remove("hideComponent");
       element.classList.add("showComponent");
-    } else {
-      element.classList.remove("showComponent");
-      element.classList.add("hideComponent");
     }
+    // else {
+    //   element.classList.remove("showComponent");
+    //   element.classList.add("hideComponent");
+    // }
   }
   onSelect(event, item) {
+    this.ProductAddSet();
     let tmp = Object.assign(this.model, item);
     this.service.changeProductsData(tmp);
   }
