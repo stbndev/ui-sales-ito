ca77a287dc65a14afe289cb68d057dd51a65d164
STBN <stbndev@mefistoles.devil>
2020-04-16 19:54:05 -0500

    20200416.1953

--- src/app/inventories/nvtrsentries/nvtrsentries.component.ts
+++ src/app/inventories/nvtrsentries/nvtrsentries.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Entriesmodel } from 'src/app/models/entriesmodel';
 import { ConfigService } from 'src/app/config/config-service.service';
+import { Productsmodel } from 'src/app/models/productsmodel';
 
 @Component({
   selector: 'app-nvtrsentries',
@@ -9,30 +10,47 @@ import { ConfigService } from 'src/app/config/config-service.service';
 })
 export class NvtrsentriesComponent implements OnInit {
   dataSource: Entriesmodel[];
+  dataProducts: Productsmodel[];
   displayedColumns: string[] = [
-    
     'identries',
     'idproducts',
-    // 'total',
-    // 'unitary_cost',
-    // 'unitary_price',
-    // 'quantity',
-    // 'existence',
-    // 'id',
-    // 'idcstatus',
-    // 'idcompany',
-    // 'maker',
-    // 'date_add',
-    // 'date_set'
+    'name',
+    'total',
+    'unitary_cost',
+    'unitary_price',
+    'quantity',
+    'existence',
+    'idcstatus',
+    'idcompany',
+    'maker',
+    'date_add',
+    'date_set'
   ];
 
   constructor(protected service: ConfigService) { }
 
   ngOnInit() {
+    this.service.Get('products').subscribe(dp => {
+      if (dp.response) {
+        this.dataProducts = dp.result;
+        // console.dir(this.dataProducts);
+      }
+    }, e => {
+      alert(e);
+    });
+
     this.service.Get('entries').subscribe(
       data => {
         if (data.response) {
           this.dataSource = data.result;
+
+          // this.dataSource[2] =
+           let tmpname = this.dataProducts.filter(function (pn) {
+            if(this.dataSource[1] == this.dataProducts[4]){
+              return pn.name;  
+            }
+          });
+          console.dir(this.dataSource)
         }
       },
       error => {
